2f6808e776862c4228ee6c828cb93e0f9a3abfc9
kaldykristof <53399030+kaldykristof@users.noreply.github.com>
2020-09-24 16:10:55 +0200

    Change unit source file types to JSON

--- run_test.sh
+++ run_test.sh
@@ -3,12 +3,12 @@
 touch test/results.txt 
 > test/results.txt
 
-./a.out unit_1.txt unit_2.txt >> test/results.txt
-./a.out unit_1.txt unit_3.txt >> test/results.txt
-./a.out unit_2.txt unit_1.txt >> test/results.txt
-./a.out unit_2.txt unit_3.txt >> test/results.txt
-./a.out unit_3.txt unit_1.txt >> test/results.txt
-./a.out unit_3.txt unit_2.txt >> test/results.txt
+./a.out unit_1.json unit_2.json >> test/results.txt
+./a.out unit_1.json unit_3.json >> test/results.txt
+./a.out unit_2.json unit_1.json >> test/results.txt
+./a.out unit_2.json unit_3.json >> test/results.txt
+./a.out unit_3.json unit_1.json >> test/results.txt
+./a.out unit_3.json unit_2.json >> test/results.txt
 
 DIFF=$(diff test/results.txt test/expected_results.txt)
 
